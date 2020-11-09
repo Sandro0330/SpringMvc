9f6fcec39bedf846a3a93ec2695217aca1bbca84
Sandro0330 <sandro030486@gmail.com>
2020-11-09 15:28:57 -0300

    Usando JPA

--- src/main/java/br/com/sandro/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/sandro/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,11 @@
 package br.com.sandro.mvc.mudi.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,19 +15,19 @@ import br.com.sandro.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
-
+	
+	//Comunicando com o Banco de Dados
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Celular Xiaomi Redmi 9 64GB");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/6147ZT4PeFL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-64GB-Dual/dp/B08B3VCF9G/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2V7TXU9LUV3SI&dchild=1&keywords=celular+xiaomi&qid=1604851903&sprefix=celuar%2Caps%2C272&sr=8-1");
-		pedido.setDescricao("Smartphone Xioami Redmi 9\r\n" + 
-				"4GB RAM e 64GB de memória interna - memória expansível (MicroSD) até 512GB.");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List pedidos = query.getResultList();
+		
 		model.addAttribute("pedidos", pedidos);
-		
+		 
 		return "home";
 		
 		
