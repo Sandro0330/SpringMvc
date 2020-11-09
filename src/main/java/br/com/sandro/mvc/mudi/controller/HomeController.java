540b0e5b2a8af48df309860c04f00a3a8f674082
Sandro0330 <sandro030486@gmail.com>
2020-11-09 16:00:41 -0300

    Injetando dependências

--- src/main/java/br/com/sandro/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/sandro/mvc/mudi/controller/HomeController.java
@@ -3,29 +3,24 @@ package br.com.sandro.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.sandro.mvc.mudi.model.Pedido;
+import br.com.sandro.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	//Comunicando com o Banco de Dados
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository repository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List pedidos = query.getResultList();
-		
+		List<Pedido> pedidos  = repository.recuperaTodosOsPedidos();
 		model.addAttribute("pedidos", pedidos);
 		 
 		return "home";
