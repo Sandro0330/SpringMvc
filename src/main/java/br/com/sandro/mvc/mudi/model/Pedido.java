e1761c2279d85aa4d00a46311810de39e4499614
Sandro0330 <sandro030486@gmail.com>
2020-11-10 13:14:46 -0300

    Configurando o menu de navegação

--- src/main/java/br/com/sandro/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/sandro/mvc/mudi/model/Pedido.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 public class Pedido {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
